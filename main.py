7f8019b7645fabad9960011c7e248e58ddd07844
evanpreet001 <evanpreetsingh2@gmail.com>
2025-05-16 20:44:43 +0530

    Update main.py
    I added the enemy's weapon throwing capabilities. Which can collide with Naruto and his shurikin and decrease his life. I managed the throwing timing of enemy's weapon so that sasuke couldn't throw weapon continuously without any space between them.

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ light_orange=(251,140,0)
 light_blue=(63,81,181)
 butn_back_colour=(0,121,107)
 gravity=1
+enwpn_delay=20
+enwpn_count=0
+
 class player:
     def __init__(self):
         #game character position and other capabilities 
@@ -57,7 +60,7 @@ class player:
             elif self.right_direction and not self.is_jumping and not self.left and not self.right :
                 win.blit(walkright[2],(self.x,self.y))
         else:
-                win.blit(pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/Nd.png'),(self.x,self.y))
+                win.blit(pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/Nd.png'),(self.x,self.y))
                 win.blit(GOF.render('GAME OVER',True,light_blue),(160,550))
                 if not self.game_over_played:
                     pygame.mixer.music.stop()
@@ -70,6 +73,7 @@ class player:
         win.blit(Nh,(150,355))
     def hit(self):
         self.health-=5
+        
 class weapon:
     def __init__(self,x,y,direction):
         self.x=x
@@ -80,24 +84,27 @@ class weapon:
         self.speed=20
         self.hitbox=(self.x,self.y,self.height,self.width)
     def draw(self,win):
-        win.blit(pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/shur.png'),(self.x,self.y))
+        win.blit(pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/shur.png'),(self.x,self.y))
         self.hitbox=(self.x,self.y,self.height,self.width)
-        #pygame.draw.rect(win,(red),self.hitbox,4)
+    def sasuke_weapon(self,win):
+        win.blit(pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/s2.png'),(self.x,self.y))
+        self.hitbox=(self.x,self.y,self.height,self.width)
         
 class enemy:
-    walkleft=[pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/SL2.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/SL3.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/SL1.png')]
-    walkright=[pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/SR2.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/SR3.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/SR1.png')]
-    dead=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/Sd.png')
+    walkleft=[pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/SL2.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/SL3.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/SL1.png')]
+    walkright=[pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/SR2.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/SR3.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/SR1.png')]
+    dead=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/Sd.png')
     def __init__(self):
         self.x=750
         self.y=750
         self.height=100
         self.width=100
-        self.initial_speed=10
         self.speed=10
         self.end=150
         self.path=[self.x,self.end]
         self.walkcount=0
+        self.left_direction=False
+        self.right_direction=False
         self.hitbox=(self.x+10,self.y+5,80,80)
         self.health=200
     def draw(self,win):
@@ -119,7 +126,6 @@ class enemy:
             if not self.you_win_played:
                 pygame.mixer.music.stop()
                 you_win.play()
-                you_win.play()
                 self.you_win_played=True 
         Sbar2=pygame.draw.rect(win,red,(570,390,200,20))
         Sbar1=pygame.draw.rect(win,yellow,(575,395,self.health,10))
@@ -128,12 +134,16 @@ class enemy:
         if self.speed>0:
             if self.x+self.speed<self.path[0]:
                 self.x+=self.speed
+                self.right_direction=True
+                self.left_direction=False
             else:
                 self.speed=self.speed*(-1)
                 self.walkcount=0
         else:
             if self.x+self.speed>self.path[1]:
                 self.x+=self.speed
+                self.right_direction=False
+                self.left_direction=True
             else:
                 self.speed=self.speed*-1
                 self.walkcount=0
@@ -151,21 +161,22 @@ font=pygame.font.SysFont(None,30,bold=False,italic=False)
 CF=pygame.font.SysFont('comicsans',170,True)
 GOF=pygame.font.SysFont('comicsans',160,True)
 #images
-bg=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/bg.png')
+bg=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/bg.png')
 shurikins=[]#empty list
-walkleft=[pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NL2.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NL3.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NL1.png')]
-jump_left=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NL4.png')
-walkright=[pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NR2.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NR3.png'),pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NR1.png')]
-jump_right=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/NR4.png')
-stand=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/Nstanding.png')
-Nh=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/Nh.png')
-Sh=pygame.image.load('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/Sh.png')
+enemy_weapon=[]
+walkleft=[pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NL2.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NL3.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NL1.png')]
+jump_left=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NL4.png')
+walkright=[pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NR2.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NR3.png'),pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NR1.png')]
+jump_right=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/NR4.png')
+stand=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/Nstanding.png')
+Nh=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/Nh.png')
+Sh=pygame.image.load('/storage/emulated/0/Narutovssasuke/pics/Sh.png')
 #sounds
-shurikinsound=pygame.mixer.Sound('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/shuriken.wav')
-hitsound=pygame.mixer.Sound('/storage/emulated/0/Ninja images /Naruto-vs-Sasuke-Pygame-main/Naruto-vs-Sasuke-Pygame-main/pics/hit.wav')
-bgm=pygame.mixer.music.load('/storage/emulated/0/Download/noncopyright-music-pianos-295174.mp3')
-game_over=pygame.mixer.Sound('/storage/emulated/0/Download/cartoon-trombone-sound-effect-241387.mp3')
-you_win=pygame.mixer.Sound('/storage/emulated/0/Download/you-win-female-gfx-sounds-3-3-00-01.mp3')
+shurikinsound=pygame.mixer.Sound('/storage/emulated/0/Narutovssasuke/pics/shuriken.wav')
+hitsound=pygame.mixer.Sound('/storage/emulated/0/Narutovssasuke/pics/hit.wav')
+bgm=pygame.mixer.music.load('/storage/emulated/0/Narutovssasuke/pics/bgm.wav')
+game_over=pygame.mixer.Sound('/storage/emulated/0/Narutovssasuke/pics/game-over.wav')    
+you_win=pygame.mixer.Sound('/storage/emulated/0/Narutovssasuke/pics/you-win.wav')
 # music play 
 pygame.mixer.music.play()
 #button  positions and sizes
@@ -185,6 +196,8 @@ def redrawgamewindow():
     sasuke.draw(win)
     for shurikin in shurikins:
         shurikin.draw(win)
+    for enwpn in enemy_weapon:
+        enwpn.sasuke_weapon(win)
 #start game running 
 while run:
     #add background 
@@ -206,7 +219,7 @@ while run:
                 naruto.y=750
                 naruto.health=200
                 sasuke.health=200
-                sasuke.speed=sasuke.initial_speed
+                sasuke.speed=10
                 sasuke.x=750
                 sasuke.y=750
                 pygame.mixer.music.play()
@@ -245,6 +258,21 @@ while run:
      if naruto.y>750:
        naruto.y=750
        naruto.is_jumping=False 
+#sasuke weapon initialise 
+     if sasuke.health>0:
+         if sasuke.left_direction :
+            enwpn_count+=1
+            if enwpn_count==enwpn_delay:
+                            enemy_weapon.append(weapon(round(sasuke.x+50),round(sasuke.y+20),'left'))
+                            enwpn_count=0
+                            shurikinsound.play()
+         elif sasuke.right_direction:
+              enwpn_count+=1
+              if enwpn_count==enwpn_delay:
+                            enemy_weapon.append(weapon(round(sasuke.x+15),round(sasuke.y+20),'right'))
+                            enwpn_count=0
+                            shurikinsound.play()
+         
     
     # win.blit(naruto,(x,y))
     # draw button
@@ -268,23 +296,46 @@ while run:
      #Naruto max an min position 
      naruto.x=max(150,min(naruto.x,750))
      naruto.y=max(350,min(naruto.y,750))
+     #naruto and sasuke collision 
      if naruto.health>0 and sasuke.health>0:
              if naruto.hitbox[0]+naruto.hitbox[2]>sasuke.hitbox[0] and naruto.hitbox[0]<sasuke.hitbox[0]+sasuke.hitbox[2]and naruto.hitbox[1]+naruto.hitbox[3]>sasuke.hitbox[1]and naruto.hitbox[1]<sasuke.hitbox[1]+sasuke.hitbox[3]:
                      naruto.hit()
                      hitsound.play()
+    #naruto weapon and enemy weapon collision 
+     for shurikin in shurikins :
+      for enwpn in enemy_weapon:
+             if shurikin.hitbox[0]+shurikin.hitbox[2]>enwpn.hitbox[0] and shurikin.hitbox[0]<enwpn.hitbox[0]+enwpn.hitbox[2]and shurikin.hitbox[1]+shurikin.hitbox[3]>enwpn.hitbox[1]and shurikin.hitbox[1]<enwpn.hitbox[1]+enwpn.hitbox[3]:
+                     hitsound.play()
+                     shurikins.pop(shurikins.index(shurikin))
+                     enemy_weapon.pop(enemy_weapon.index(enwpn))
+    #naruto weapon and sasuke collision 
      for shurikin in shurikins:
          if sasuke.health>0:
              if shurikin.hitbox[0]+shurikin.hitbox[2]>sasuke.hitbox[0] and shurikin.hitbox[0]<sasuke.hitbox[0]+sasuke.hitbox[2]and shurikin.hitbox[1]+shurikin.hitbox[3]>sasuke.hitbox[1]and shurikin.hitbox[1]<sasuke.hitbox[1]+sasuke.hitbox[3]:
                      sasuke.hit()
                      hitsound.play()
                      shurikins.pop(shurikins.index(shurikin))
+        #naruto weapon function 
          if shurikin.direction=='right':
                 shurikin.x+=shurikin.speed
          if shurikin.direction=='left':
                 shurikin.x-=shurikin.speed
          if shurikin.x<150 or shurikin.x>800:
              shurikins.pop(shurikins.index(shurikin))
+    #sasuke weapon and naruto collision 
+     for enwpn in enemy_weapon:
+         if naruto.health>0:
+             if enwpn.hitbox[0]+enwpn.hitbox[2]>naruto.hitbox[0] and enwpn.hitbox[0]<naruto.hitbox[0]+naruto.hitbox[2]and enwpn.hitbox[1]+enwpn.hitbox[3]>naruto.hitbox[1]and enwpn.hitbox[1]<naruto.hitbox[1]+naruto.hitbox[3]:
+                     naruto.hit()
+                     hitsound.play()
+                     enemy_weapon.pop(enemy_weapon.index(enwpn))
+        #sasuke weapon function 
+         if enwpn.direction=='right':
+                enwpn.x+=enwpn.speed
+         if enwpn.direction=='left':
+                enwpn.x-=enwpn.speed
+         if enwpn.x<150 or enwpn.x>800:
+             enemy_weapon.pop(enemy_weapon.index(enwpn))
      redrawgamewindow()
      pygame.display.update()#update the window after any change
 pygame.quit()
-
